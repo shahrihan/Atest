4d7a2264161c3ce4d80a1d089041b8a793e22b9e
shahrihan <behruzahon7@gmail.com>
2017-08-26 21:49:47 -0400

    Next version

--- src/aTest/Apple.java
+++ src/aTest/Apple.java
@@ -3,7 +3,7 @@ package aTest;
 public class Apple {
 	public static void main(String[] args) {
 		System.out.println("I love an apple ");
-		System.out.println();
+		System.out.println("++++++++++++");
 		
 	}
 
